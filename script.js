22d1e4e78543b051ecdb4c772c6740e88f848e84
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-07-29 05:16:05 +0500

    Refactor gameBoard module to include get and addMark methods for improved functionality

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const gameBoard = (() => {
   const EMPTY_BOARD = [``, ``, ``, ``, ``, ``, ``, ``, ``];
-
   let board = [...EMPTY_BOARD];
+
+  const get = () => [...board];
+
+  const addMark = (mark, index) => {
+    if (typeof mark !== `string` || mark === ``) return false;
+    if (typeof index !== "number" || isNaN(index)) return false;
+    if (mark !== `X` && mark !== `O`) return false;
+    if (index < 0 || index > 8) return false;
+
+    board[index] = mark;
+    return true;
+  };
 
   const reset = () => {
     board = [...EMPTY_BOARD];
   };
 
-  return { reset };
+  return { get, addMark, reset };
 })();
 
 const playerFactory = (name, marker) => {
@@ -31,4 +42,5 @@ const gameController = (() => {
   return { init, playRound };
 })();
 
-gameController.init();
+gameBoard.addMark(`X`, 0);
+console.log(gameBoard.get());
